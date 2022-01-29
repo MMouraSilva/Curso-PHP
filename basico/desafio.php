edf3ebf28707f80372153b9a73d5de26a93bf599
Matheus Moura <matheusmoura78@hotmail.com>
2022-01-29 12:23:24 -0300

    Aula Desafio

---
+++ basico/desafio.php
@@ -0,0 +1,7 @@
+<div class="titulo">Desafio</div>
+
+<ul>
+    <li>1 + 1 = <?= 1+1?></li>
+    <li>4 + 4 = <?php echo 4+4; ?></li>
+    <li><a href="exercicio.php?dir=basico&file=desafio&num1=8&num2=8">8 + 8 = </a> <?= $_GET['num1'] + $_GET['num2'] ?></li>
+</ul>
