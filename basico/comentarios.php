f14abc9d558298340b2a3065e5b9ba1db533bde7
Matheus Moura <matheusmoura78@hotmail.com>
2022-01-29 11:41:23 -0300

    Aula Comentários

---
+++ basico/comentarios.php
@@ -0,0 +1,26 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+    echo "Estou no PHP";
+    //echo "Comentário";
+    # echo "Comentário";
+    #echo //"Comentário";
+    //echo #"Comentário";
+# ?>
+
+<p>Depois do primeiro bloco!</p>
+<!--   
+    <p>Comnetário HTML 1</p>
+    <p>Comnetário HTML 2</p>
+    <p>Comnetário HTML 3</p>
+ -->
+
+<?php
+/*
+    Linha 1 do comentário
+    Linha 2 do comentário
+    Linha 3 do comentário
+*/      
+?>
+
+<p>Depois do segundo bloco!</p>
--- index.php
+++ index.php
@@ -33,6 +33,11 @@
                                 Integração CSS
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">
+                                Comentários PHP
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
