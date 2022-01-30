bc080d8e79340944eb1f238a61e6b5b4f983a168
Matheus Moura <matheusmoura78@hotmail.com>
2022-01-30 01:39:23 -0300

    Aula Variaveis Variaveis

--- index.php
+++ index.php
@@ -113,6 +113,16 @@
                                 Interpolação
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=interpolacao">
+                                Interpolação
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">
+                                Variáveis Variáveis
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 </nav>
