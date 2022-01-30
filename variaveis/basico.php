f66e8fa9cb57fe44e9e547fe4a400fccabf0231c
Matheus Moura <matheusmoura78@hotmail.com>
2022-01-30 00:37:36 -0300

    Aula Variaveis

---
+++ variaveis/basico.php
@@ -0,0 +1,39 @@
+<div class="titulo">Variáveis</div>
+
+<?php
+$numeroA = 13;
+echo $numeroA;
+echo '<br>';
+var_dump($numeroA);
+
+echo '<br>';
+$a = 3;
+$b = 16;
+$somaDosNumeros = $a + $b;
+echo $somaDosNumeros;
+echo '<br>';
+echo isset($somaDosNumeros);
+echo '<br>';
+
+unset($somaDosNumeros);
+var_dump($somaDosNumeros);
+
+echo '<br>';
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = "Agora sou uma string!";
+echo '<br>' . $variavel;
+
+// Nomes de variaveis
+$var = 'valida';
+$var2 = 'valida';
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+$vár5 = 'valida'; // evitar
+// $6var = 'invalida';
+// $%var7 = 'invalida';
+// $var8% = 'invalida';
+
+echo '<br>';
+var_dump($_SERVER["HTTP_HOST"]);
