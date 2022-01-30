a712bf73b6badcad761165a3f8f5d4aa1c281798
Matheus Moura <matheusmoura78@hotmail.com>
2022-01-29 21:38:05 -0300

    Desafio String

---
+++ tipos/desafio_string.php
@@ -0,0 +1,12 @@
+<div class="titulo">Desafio String</div>
+
+<?php
+
+// Enunciado:
+// Avaliando os métodos da documentação da string,
+// qual o método que ira retornar a posição do texto 
+// ,'abc' na string, '!AbcaBcabc', como 1?
+
+echo stripos('!AbcaBcabc', 'abc');
+
+?>
